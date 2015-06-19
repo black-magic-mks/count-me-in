525aeb39230d2aa3aea1585d8de77b384b6300dd
TheRealest <real@rrprovencher.com>
2015-06-19 15:48:12 -0700

    [chore]: fixed concat separator in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     concat: {
       options: {
-        separator: ' '
+        separator: ';'
       },
       dist: {
         src: ['client/**/*.js'],
