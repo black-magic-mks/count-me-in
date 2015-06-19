bb58b410386cbbe4ac9dd3639c18932882fcfdab
Shaan Shah <shahshaan@gmail.com>
2015-06-18 17:44:09 -0700

    [chore]: created top-level files for bower, npm, gitignore, and grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['client/**/*.js'],
+        dest: 'public/scripts.js'
+      },
+    },
+    watch: {
+      files: ['client/**/*.js'],
+      tasks: ['concat']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['concat', 'watch']);
+
+};
