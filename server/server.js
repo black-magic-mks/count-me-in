861b6a4222be5f58ff6601e3646d12bd5ed3896c
TheRealest <real@rrprovencher.com>
2015-06-20 15:10:24 -0700

    [feat]: added seraph/seraph-model and example User model

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = require('./router');
 
 var port = 8080;
 var app = express();
 
+
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
 
 router.addRoutes(app);
