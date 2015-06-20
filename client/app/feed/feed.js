5abf7b5681a1f02585d2f176a94abcc8e3d33f06
Nathan Lin <nlin.wei.an@gmail.com>
2015-06-19 17:18:35 -0700

    [refactor]: added test code for feedPledge and feedPledgePost directives

--- client/app/feed/feed.js
+++ client/app/feed/feed.js
@@ -1,5 +1,34 @@
 angular.module('app')
 
 .controller('FeedController', function($scope) {
-
+  $scope.pledgeCategories = [{
+    name: 'yolo',
+    pledges: [{
+      user: {
+        mission: "sup bro",
+        date: "09-18-1992",
+        posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
+      },
+      user: {
+        mission: "sup bro",
+        date: "09-18-1992",
+        posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
+      }
+    }]
+  },
+  {
+    name: 'Hello',
+    pledges: [{
+      user: {
+        mission: "sup bro",
+        date: "09-18-1992",
+        posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
+      },
+      user: {
+        mission: "sup bro",
+        date: "09-18-1992",
+        posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
+      }
+    }]
+  }]
 });
