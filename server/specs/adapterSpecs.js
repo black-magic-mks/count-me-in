fdba2f565cfc6a74f82e25f0fda21482f31b2847
David Goldberg <drgold1988@gmail.com>
2015-06-21 12:28:44 -0700

    [test]: creates files for server-side tests

---
+++ server/specs/adapterSpecs.js
@@ -0,0 +1,4 @@
+var authAdapter = require('../db/adapter/authAdapter');
+var pledgeAdapter = require('../db/adapter/pledgeAdapter');
+var postAdapter = require('../db/adapter/postAdapter');
+var userAdapter = require('../db/adapter/userAdapter');
