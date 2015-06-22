d3acb4f78bb85eaaf56068df44ea6ed8e8e45db7
Nathan Lin <nlin.wei.an@gmail.com>
2015-06-22 12:06:28 -0700

    [feat]: added auth controller and factory functions for logIn, logOut, register, and isLoggedIn

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,10 +1,70 @@
 angular.module('app')
 
-.controller('AuthController', function($scope) {
+.controller('authController', function($scope, Auth, $location, $window) {
   $scope.user = {};
   $scope.passwordValidation = /.*(\d(?=.*[A-Z])|[A-Z](?=.*\d)).*/;
- })
 
-.controller('LoginController', function($scope) {
+  angular.extends($scope, Auth);
 
-});
+  $scope.logIn = function() {
+    Auth.logIn($scope.username, $scope.password)
+    .then(function(token) {
+      $window.localStorage.setItem('loggedIn', token);
+      $location.path('./i/feed');
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  }
+
+  $scope.register = function() {
+    Auth.register($scope.username, $scope.password)
+    .then(function(token) {
+      $window.localStorage.setItem('loggedIn', token);
+      $location.path('./i/feed');
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  }
+})
+
+.factory('Auth', function($http, $location, $window) {
+  var logIn = function(username, password) {
+    return $http({
+      method: 'POST',
+      url: '/api/auth/login',
+      data: {username: username, password: password}
+    })
+    .then(function(res) {
+      return res.data.token;
+    });
+  }
+
+  var register = function(username, password) {
+    return $http({
+      method: 'POST',
+      url: '/api/auth/register',
+      data: {username: username, password: password}
+    })
+    .then(function(res) {
+      return res.data.token;
+    });
+  }
+
+  var logOut = function() {
+    $window.localStorage.removeItem('loggedIn');
+    $location.path('./i/feed');
+  }
+
+  var isLoggedIn = function() {
+     return !!$window.localStorage.getItem('loggedIn');
+  }
+
+  return {
+    logIn: logIn,
+    register: register,
+    logOut: logOut,
+    isLoggedIn: isLoggedIn
+  }
+})
