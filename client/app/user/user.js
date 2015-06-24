ac4ee6e0130aa913f81ebc1f6876367f880dc97c
David Goldberg <drgold1988@gmail.com>
2015-06-24 15:41:09 -0700

    [fix]: removes unnecessary comments in user.js

--- client/app/user/user.js
+++ client/app/user/user.js
@@ -1,31 +1,28 @@
 angular.module('app')
 
 .controller('UserController', function($scope, userFunc) {
-  // angular.extend($scope, userFunc);
   $scope.user = userFunc;
   $scope.pledgePreview = [];
 
-  userFunc.getUser();
+  userFunc.getUser(null, function(data) {
+    $scope.username = data.username;
+  });
   userFunc.getUserPledges(null, function(data) {
-    $scope.pledgePreview = $scope.pledgePreview.concat(data);
+    $scope.pledgePreview = $scope.pledgePreview.concat (data);
     console.log(data,$scope.pledgePreview);
   });
-  // userFunc.getUserPledges(function(pledgePreview) {
-  //   $scope.pledgePreview = pledgePreview;
-  // });
+
   console.log('pledgePreview in controller: ', $scope.pledgePreview);
 })
 .factory('userFunc', function($http) {
-  // var pledgePreview = [];
   var username;
-  var getUser = function(username) {
+  var getUser = function(username, cb) {
     $http.get('/api/user', {
       params: {username: username}
     })
     .success(function(data, status, headers, config) {
       username = data.username;
-      console.log('getUser: ', data);
-      console.log('username to show: ', data.username);
+      cb(data);
     })
     .error(function(data, status, headers, config) {
       console.log('error with get request for getUser');
@@ -36,8 +33,7 @@ angular.module('app')
       params: {username: username}
     })
     .success(function(data, status, headers, config) {
-      console.log('getUserPledges: ', data);
-          cb(data);
+      cb(data);
     })
     .error(function(data, status, headers, config) {
       console.log('error with get request for getUserPledges');
