fabd9d091a5466024fb1a0b8b1d5708909b93168
David Goldberg <drgold1988@gmail.com>
2015-06-23 19:45:12 -0700

    [feat]: queries get requests communicating with back-end

--- client/app/feed/feed.js
+++ client/app/feed/feed.js
@@ -2,7 +2,7 @@ angular.module('app')
 
 .controller('FeedController', function($scope, feedFunc) {
   angular.extend($scope, feedFunc);
-  $scope.getFollowedPledges('therealest');
+  $scope.getFollowedPledges('mengel');
   $scope.getPledgePosts('code');
 })
 .factory('feedFunc', function($http) {
@@ -10,12 +10,10 @@ angular.module('app')
   var userPledges = [];
   var getFollowedPledges = function(username) {
     $http.get('/api/user/pledges', {
-    params: {username: username}
+      params: {username: username}
     }).
     success(function(data, status, headers, config) {
-      // // Format data
-      // pledgeCategories.push(data);
-      console.log('getFollowedPledges :', data);
+    
     }).
     error(function(data, status, headers, config) {
       console.log('error with get request for api/user/pledges');
@@ -23,16 +21,15 @@ angular.module('app')
   }
 
   var getPledgePosts = function(pledgename) {
-    console.log('getPledgePosts is called');
+    console.log('pledgename',pledgename);
     $http.get('/api/pledge/posts', {
       params: {pledgename: pledgename}
-    }).
-    success(function(data, status, headers, config) {
-      // userPledges.push(data);
-      console.log('getpledgeposts :', data);
+    })
+    .success(function(data, status, headers, config) {
+
     }).
     error(function(data, status, headers, config) {
-      console.log('error with get request for api/pledge/posts');
+      console.log('error with get request for api/pledge/posts',data,status,headers,config);
     });
   }  
   
--- server/server.js
+++ server/server.js
@@ -8,12 +8,18 @@ var port = 8080;
 var app = express();
 
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 app.use(multiparty);
 app.use(express.static(__dirname + '/../public'));
 app.get('/favicon.ico', function(_, res) { res.type('image/x-icon').end(); });
 
+app.use(function(req,res,next) {
+  if (Object.keys(req.body).length === 0) req.body = req.query;
+  next();
+});
+
 app.use(auth);
-router.addRoutes(app);
+app.use('/api',router);
 
 app.use(function(err, req, res, next) {
   console.error(err);
