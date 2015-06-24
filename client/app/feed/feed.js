02e9ce3d6acec04f8a90d4b43b686218a0ce9f98
Monica Engel <monicaengel9@gmail.com>
2015-06-24 14:45:42 -0700

    [feat]: rendered feed/all/pledge page info from db data

--- client/app/feed/feed.js
+++ client/app/feed/feed.js
@@ -1,7 +1,7 @@
 angular.module('app')
 
 .controller('FeedController', function($scope, $ionicModal, feedFunc) {
-  angular.extend($scope, feedFunc);
+  // angular.extend($scope, feedFunc);
 
   $scope.pledgeCategories = [
   {
@@ -39,20 +39,35 @@ angular.module('app')
    ]
  }
  ];
-  $scope.graphData = {
-    name: 'coding',
-    posts: [{
-      username: '@Nathan',
-      date: '1-2-11',
-      text: 'sup bro. hello. hello. hello. hello. hello. hello. hello. hello. hello',
-      image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSfF7olOL9BDXN1uWdT2yd4ldgXJo7rbd0OTO0oGuYFHlw1dXjr-Q'
-    }]
-  };
 
-  $scope.getFollowedPledges('mengel');
-  $scope.getPledgePosts('code');
-  $scope.getPledgeView('piano');
-  // $scope.clickedPledge;
+  $scope.pledgeCategories = [];
+  $scope.graphData = {};
+  $scope.graphData.posts = [];
+
+  feedFunc.getFollowedPledges(null, function(data) {
+    console.log('getFollowedPledges', data);
+  });
+
+  feedFunc.getPledgePosts('code', function(data) {
+    console.log('pledge-getPledgePosts', data);
+    data.forEach(function(pledge) {
+      // $scope.pledgeCategories.push()
+    })
+  });
+
+  feedFunc.getPledgeView('piano', function(data) {
+    console.log(data.createdAt);
+    data.forEach(function(post) {
+      $scope.graphData.name = 'piano';
+      $scope.graphData.posts.push(post);
+      console.log('$scope.graphData', $scope.graphData);
+    })
+    console.log('got back to controller', data);
+  });
+  
+  // $scope.getFollowedPledges('mengel');
+  // $scope.getPledgePosts('code');
+  // $scope.getPledgeView('piano');
 
   $ionicModal.fromTemplateUrl('templates/modal.html', {
     scope: $scope,
@@ -66,45 +81,42 @@ angular.module('app')
   $scope.viewPledge = function() {
     //pass in clicked element
     // feedFunc.getPledgeView();
-    console.log('viewPledge running');
+    // console.log('viewPledge running');
   };
 })
 
 .factory('feedFunc', function($http) {
-  var pledgeCategories = [];
-  var userPledges = [];
-  var getFollowedPledges = function(username) {
+
+  var getFollowedPledges = function(username, callback) {
     $http.get('/api/user/pledges', {
       params: {username: username}
     })
     .success(function(data, status, headers, config) {
-      console.log('getFollowedPages', data);
+      callback(data);
     })
     .error(function(data, status, headers, config) {
       console.log('error with get request for api/user/pledges');
     });
   };
 
-  var getPledgePosts = function(pledgename) {
-    $http.get('/api/pledge/posts', {
+  var getPledgePosts = function(pledgename, callback) {
+    $http.get('/api/pledge', {
       params: {pledgename: pledgename}
     })
     .success(function(data, status, headers, config) {
-      // userPledges.push(data);
-      console.log('getPledgePosts', data);
+      callback(data);
     })
     .error(function(data, status, headers, config) {
       console.log('error with get request for api/pledge/posts',data,status,headers,config);
     });
   }; 
 
-  var getPledgeView = function(pledgename) {
+  var getPledgeView = function(pledgename, callback) {
     $http.get('/api/pledge/posts', {
       params: {pledgename: pledgename}
     })
     .success(function(data, status, headers, config) {
-      console.log('success getting getPledgeView');
-      console.log('getPledgeView pledge/posts!', data);
+      callback(data);
 
     }).error(function(data, status, headers, config) {
       console.log('effor with request');
