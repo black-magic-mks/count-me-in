09a4d26c8b71c4d2c9d7e866488711ec8381eb6f
David Goldberg <drgold1988@gmail.com>
2015-06-23 17:18:47 -0700

    [fix]: http requests in feed.js work in progress

--- client/app/feed/feed.js
+++ client/app/feed/feed.js
@@ -2,23 +2,19 @@ angular.module('app')
 
 .controller('FeedController', function($scope, feedFunc) {
   angular.extend($scope, feedFunc);
-  $scope.username = 'therealest';
-  $scope.pledgename = 'coding';
-  $scope.getFollowedPledges($scope.username);
-  $scope.getPledgePosts($scope.pledgename);
-  
+  $scope.getFollowedPledges('therealest');
+  $scope.getPledgePosts('dogs');
 })
 .factory('feedFunc', function($http) {
   var pledgeCategories = [];
   var userPledges = [];
   var getFollowedPledges = function(username) {
-    console.log('hi');
     $http.get('/api/user/pledges', {
     params: {username: username}
     }).
     success(function(data, status, headers, config) {
-      // Format data
-      pledgeCategories.push(data);
+      // // Format data
+      // pledgeCategories.push(data);
       console.log(data);
     }).
     error(function(data, status, headers, config) {
@@ -31,7 +27,7 @@ angular.module('app')
       params: {pledgename: pledgename}
     }).
     success(function(data, status, headers, config) {
-      userPledges.push(data);
+      // userPledges.push(data);
       console.log(data);
     }).
     error(function(data, status, headers, config) {
