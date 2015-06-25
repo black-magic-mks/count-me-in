69706235acc9f4da6ac646a33da8ff32c8198715
Monica Engel <monicaengel9@gmail.com>
2015-06-24 17:31:36 -0700

    [feat]: implements api-request structure for feed/all

--- client/app/feed/feedController.js
+++ client/app/feed/feedController.js
@@ -4,63 +4,72 @@ angular.module('app')
   // angular.extend($scope, feedFunc);
 
   $scope.pledgeCategories = [
-  {
-   name: '#Coding',
-   postList: [
-     {
-       user: "@Nathan",
-       mission: "I don't know what I'm doing",
-       date: "06-18-2015",
-       posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
-     },
-     {
-       user: "@Jack",
-       mission: "I'm a bad person",
-       date: "04-12-1792",
-       posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
-     }
-   ]
- },
- {
-   name: '#Piano',
-   postList: [
-     {
-       user: "@Monica",
-       mission: "I had a good idea today",
-       date: "03-21-1992",
-       posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
-     },
-     {
-       user: "@Antonio",
-       mission: "Jack is a bad person",
-       date: "09-31-1992",
-       posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
-     }
-   ]
- }
+ //  {
+ //   name: '#Coding',
+ //   postList: [
+ //     {
+ //       user: "Nathan",
+ //       mission: "I don't know what I'm doing",
+ //       date: "06-18-2015",
+ //       posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
+ //     },
+ //     {
+ //       user: "@Jack",
+ //       mission: "I'm a bad person",
+ //       date: "04-12-1792",
+ //       posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
+ //     }
+ //   ]
+ // },
+ // {
+ //   name: '#Piano',
+ //   postList: [
+ //     {
+ //       user: "@Monica",
+ //       mission: "I had a good idea today",
+ //       date: "03-21-1992",
+ //       posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
+ //     },
+ //     {
+ //       user: "@Antonio",
+ //       mission: "Jack is a bad person",
+ //       date: "09-31-1992",
+ //       posts: ['https://thenypost.files.wordpress.com/2013/11/corgi.jpg','http://www.petguide.com/wp-content/uploads/2013/02/shiba-inu1.jpg']
+ //     }
+ //   ]
+ // }
  ];
 
   $scope.pledgeCategories = [];
+
+  $scope.postListTemp = [];
   $scope.graphData = {};
   $scope.graphData.posts = [];
 
   feedFunc.getFollowedPledges('mengel', function(data) {
-    console.log('getFollowedPledges', data);
-  });
-
-  feedFunc.getPledgePosts('code', function(data) {
-    console.log('pledge-getPledgePosts', data);
     data.forEach(function(pledge) {
-      // $scope.pledgeCategories.push()
+
+      $scope.pledgeCategories.push({name: pledge.pledgename});
+      $scope.postListTemp.mission = pledge.mission;
+
+      feedFunc.getPledgePosts(pledge.pledgename, function(data){
+        
+        data.forEach(function(post) {
+          $scope.postListTemp.aws_url = post.aws_url;
+          $scope.postListTemp.username = post.username;
+        })
+
+        $scope.pledgeCategories.postList = $scope.postListTemp;
+        console.log('pledgecat', $scope.pledgeCategories);
+      
+      })
     })
   });
 
   feedFunc.getPledgeView('piano', function(data) {
-    console.log('date:', data[0].created);
     data.forEach(function(post) {
       $scope.graphData.name = 'piano';
       $scope.graphData.posts.push(post);
-      console.log('$scope.graphData', $scope.graphData);
     })
   });
 
@@ -95,7 +104,7 @@ angular.module('app')
   };
 
   var getPledgePosts = function(pledgename, callback) {
-    $http.get('/api/pledge', {
+    $http.get('/api/pledge/posts', {
       params: {pledgename: pledgename}
     })
     .success(function(data, status, headers, config) {
