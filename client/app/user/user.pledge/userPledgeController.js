b155934a486ad7a19fb6e0e3d747424bbc84801f
David Goldberg <drgold1988@gmail.com>
2015-06-26 15:53:58 -0700

    [feat]: implements get request for pledge that user subscribes to

--- client/app/user/user.pledge/userPledgeController.js
+++ client/app/user/user.pledge/userPledgeController.js
@@ -1,21 +1,44 @@
 angular.module('app')
 
-.controller('UserPledgeController', function($scope, subscribe) {
+.controller('UserPledgeController', function($scope, UserPledgeFactory, subscribe) {
+
+  // TODO: replace null with pledgename the user is subscribing to
+  UserPledgeFactory.getUserPledgeData(null, function(data) {
+    $scope.pledgename = data.pledgename;
+  });
 
   $scope.subscribedPledges = [];
 
   $scope.subscribePledge = function() {
-    subscribe.subscribeToPledge($scope.pledgename, $scope.mission, function(data) {
+    subscribe.subscribeToPledge($scope.pledgename, function(data) {
       $scope.subscribedPledges = $scope.subscribedPledges.push (data);
       console.log('subscribedPledges: ', data, $scope.subscribedPledges);
     });
   };
 })
+.factory('UserPledgeFactory', function($http) {
+  var getUserPledgeData = function(pledgename, callback) {
+    console.log('pledgename start of getUserPledgeData: ', pledgename);
+    $http.get('/api/pledge/posts', {pledgename: pledgename})
+    .success(function(data, status, headers, config) {
+      callback(data);
+      console.log('getUserPledgeData data: ', data);
+    })
+    .error(function(data, status, headers, config) {
+      console.log('THIS IS THE PLEDGENAME IN ERROR: ', pledgename);
+      console.log('error status with getUserPledgeData: ', status, data, headers, config);
+    });
+  };
+
+  return {
+    getUserPledgeData: getUserPledgeData
+  }
+
+})
 .factory('subscribe', function($http) {
-  var subscribeToPledge = function(pledgename, mission, callback) {
+  var subscribeToPledge = function(pledgename, callback) {
     console.log('pledgename: ', pledgename);
-    console.log('mission: ', mission);
-    $http.post('/api/pledge/subscribe', {pledgename: pledgename, mission: mission})
+    $http.post('/api/pledge/subscribe', {pledgename: pledgename})
     .success(function(data, status, headers, config) {
       callback(data);
       console.log('subscribeToPledge data: ', data);
