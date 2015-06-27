0a571a0fc55e0fa6c1e6a34791a5901813813084
Nathan Lin <nlin.wei.an@gmail.com>
2015-06-27 12:05:18 -0700

    [refactor]: routing tests now work correctly

--- client/specs/routingSpecs.js
+++ client/specs/routingSpecs.js
@@ -8,72 +8,93 @@ describe('Routing', function () {
     $stateParams = $injector.get('$stateParams');
   }));
 
-it('Should have \'tab.feed\' state, controller, and url', function () {
+  it('Should have \'tab.feed\' state, url, template, and controller', function () {
     var state = $state.get('tab.feed');
-    var view = state.views['tabs-feed'];
+    var view = state.views['tab-feed'];
     expect(state.name).to.be('tab.feed');
     expect(state.url).to.be('/feed');
-    expect(view.templateUrl).to.be('/views/user.html');
-    expect(view.Controller).to.be('FeedController');
+    expect(view.templateUrl).to.be('/views/feed.html');
+    expect(view.controller).to.be('FeedController');
   });
 
-  it('Should have \'tab.feed.all\' state, controller, and url', function () {
+  it('Should have \'tab.feed.all\' state, url, template, and controller', function () {
     var state = $state.get('tab.feed.all');
     expect(state.name).to.be('tab.feed.all');
     expect(state.url).to.be('/all');
-    expect(state.controller).to.be('PledgeController')
     expect(state.templateUrl).to.be('/views/feed.all.html');
+    expect(state.controller).to.be('FeedController')
   });
 
-  it('Should have \'tab.feed.pledge\' state, controller, and url', function () {
+  it('Should have \'tab.feed.pledge\' state, url, template, and controller', function () {
     var state = $state.get('tab.feed.pledge');
     expect(state.name).to.be('tab.feed.pledge');
-    expect(state.controller).to.be('PledgeController')
     expect(state.url).to.be('/pledge/:pledgename');
     expect(state.templateUrl).to.be('/views/feed.pledge.html');
+    expect(state.controller).to.be('FeedController')
   });
-  it('Should have \'tab.user\' state, view, and url', function () {
+
+  it('Should have \'tab.user\' state, url, template, and controller', function () {
     var state = $state.get('tab.user');
-    var view = state.views['tabs-user'];
+    var view = state.views['tab-user'];
     expect(state.name).to.be('tab.user');
     expect(state.url).to.be('/user/:username');
     expect(view.templateUrl).to.be('/views/user.html');
     expect(view.controller).to.be('UserController');
   });
 
-  it('Should have \'tab.user.dashboard\' state, controller, and url', function () {
+  it('Should have \'tab.user.dashboard\' state, url, and template', function () {
     var state = $state.get('tab.user.dashboard');
     expect(state.name).to.be('tab.user.dashboard');
-    expect(state.controller).to.be('UserController')
     expect(state.url).to.be('/dashboard');
     expect(state.templateUrl).to.be('/views/user.dashboard.html');
   });
-  it('Should have \'tab.user.addPost\' state, controller, and url', function () {
-    var state = $state.get('tab.user.addPost');
-    expect(state.name).to.be('tab.user.addPost');
-    expect(state.controller).to.be('UserAddPostController')
-    expect(state.url).to.be('/post/new');
-    expect(state.templateUrl).to.be('/views/user.addPost.html');
+
+  it('Should have \'tab.user.post\' state, url, and template', function () {
+    var state = $state.get('tab.user.post');
+    expect(state.name).to.be('tab.user.post');
+    expect(state.url).to.be('/post');
+    expect(state.templateUrl).to.be('/views/post.html');
   });
-  it('Should have \'tab.user.pledge\' state, controller, and url', function () {
+
+  it('Should have \'tab.user.post.add\' state, url, template, and controller', function () {
+    var state = $state.get('tab.user.post.add');
+    expect(state.name).to.be('tab.user.post.add');
+    expect(state.url).to.be('/new');
+    expect(state.templateUrl).to.be('/views/post.add.html');
+    expect(state.controller).to.be('addPostController')
+  });
+
+  it('Should have \'tab.user.post.view\' state, url, template, and controller', function () {
+    var state = $state.get('tab.user.post.view');
+    expect(state.name).to.be('tab.user.post.view');
+    expect(state.url).to.be('/:post_id');
+    expect(state.templateUrl).to.be('/views/post.view.html');
+    expect(state.controller).to.be('viewPostController')
+  });
+
+  it('Should have \'tab.user.pledge\' state, url, template, and controller', function () {
     var state = $state.get('tab.user.pledge');
     expect(state.name).to.be('tab.user.pledge');
-    expect(state.controller).to.be('UserPledgeController')
     expect(state.url).to.be('/:pledgename');
     expect(state.templateUrl).to.be('/views/user.pledge.html');
+    expect(state.controller).to.be('UserPledgeController')
   });
-  it('Should have \'tab.login\' state, controller, and url', function () {
+
+  it('Should have \'tab.login\' state, url, template, and controller', function () {
     var state = $state.get('tab.login');
+    var view = state.views['tab-login'];
     expect(state.name).to.be('tab.login');
-    expect(state.controller).to.be('AuthController')
     expect(state.url).to.be('/login');
-    expect(state.templateUrl).to.be('/views/login.html');
+    expect(view.templateUrl).to.be('/views/login.html');
+    expect(view.controller).to.be('AuthController')
   });
-  it('Should have \'tab.signup\' state, controller, and url', function () {
+
+  it('Should have \'tab.signup\' state, url, template, and controller', function () {
     var state = $state.get('tab.signup');
+    var view = state.views['tab-signup'];
     expect(state.name).to.be('tab.signup');
-    expect(state.controller).to.be('AuthController')
     expect(state.url).to.be('/signup');
-    expect(state.templateUrl).to.be('/views/signup.html');
+    expect(view.templateUrl).to.be('/views/signup.html');
+    expect(view.controller).to.be('AuthController')
   });
 });
