300d7032faebcebc6c31f197d6a8dfbf0d978ddc
Monica Engel <monicaengel9@gmail.com>
2015-07-01 14:32:45 -0700

    [fix]:fixed ionic tab bar to correctly render pages and remove bug.

--- client/app/app.js
+++ client/app/app.js
@@ -35,7 +35,7 @@ angular.module('app', [
     .state('tab.user', {
       url: '/user',
       views: {
-        'tab-user-profile': {
+        'tab-user': {
           templateUrl: '/views/user.html',
           controller: 'UserController'
         }
@@ -49,40 +49,78 @@ angular.module('app', [
       url: '/following',
       templateUrl: '/views/user.profile.html'
     })
-    .state('tab.user.post', {
+
+     .state('tab.post', {
       url: '/:username/post',
       views: {
-        'tab-user-post': {
+        'tab-post': {
           templateUrl: '/views/post.html',
           controller: 'addPostController'
         }
       }
     })
-    .state('tab.user.post.add', {
+    .state('tab.post.add', {
       url: '/new',
-      templateUrl: '/views/post.add.html',
-      controller: 'addPostController'    
+      templateUrl: '/views/post.add.html'
     })
-    .state('tab.user.post.view', {
+    .state('tab.post.view', {
       url: '/:post_id',
       templateUrl: '/views/post.view.html',
-      controller: 'viewPostController'
+      controller: 'viewPostController' // in feedController.js for now; make separate file when you can come up with a good name for the file or when we seperate things into factory and controller files
     })
-    .state('tab.user.pledge', {
+    .state('tab.post.pledge', {
       url: '/:username/:pledgename',
       templateUrl: '/views/user.pledge.html',
       controller: 'UserPledgeController'
     })
-    .state('tab.user.pledge.add', {
+    .state('tab.post.pledge.add', {
       url: '/new',
       templateUrl: '/views/user.pledge.add.html',
       controller: 'addPledgeController'
     })
-    .state('tab.user.pledge.view', {
+    .state('tab.post.pledge.view', {
       url: '/view',
       templateUrl: '/views/user.pledge.view.html',
       controller: 'viewPledgeController'
     })
+
+
+
+    // .state('tab.user.post', {
+    //   url: '/:username/post',
+    //   views: {
+    //     'tab-user-post': {
+    //       templateUrl: '/views/post.html',
+    //       controller: 'addPostController'
+    //     }
+    //   }
+    // })
+    // .state('tab.user.post.add', {
+    //   url: '/new',
+    //   templateUrl: '/views/post.add.html',
+    //   controller: 'addPostController'    
+    // })
+    // .state('tab.user.post.view', {
+    //   url: '/:post_id',
+    //   templateUrl: '/views/post.view.html',
+    //   controller: 'viewPostController'
+    // })
+    // .state('tab.user.pledge', {
+    //   url: '/:username/:pledgename',
+    //   templateUrl: '/views/user.pledge.html',
+    //   controller: 'UserPledgeController'
+    // })
+    // .state('tab.user.pledge.add', {
+    //   url: '/new',
+    //   templateUrl: '/views/user.pledge.add.html',
+    //   controller: 'addPledgeController'
+    // })
+    // .state('tab.user.pledge.view', {
+    //   url: '/view',
+    //   templateUrl: '/views/user.pledge.view.html',
+    //   controller: 'viewPledgeController'
+    // })
+
     .state('tab.login', {
       url: '/login',
       views: {
