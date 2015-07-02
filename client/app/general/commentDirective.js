d5625a3994bfd90b82a15af64c5acfef2780dffa
Monica Engel <monicaengel9@gmail.com>
2015-07-02 12:10:22 -0700

    [feat]: updated postId to display comments on correct posts

--- client/app/general/commentDirective.js
+++ client/app/general/commentDirective.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.directive('comment', function($http) {
+.directive('comment', function($http, $rootScope) {
   return {
     restrict: 'E',
     scope: {
@@ -10,20 +10,25 @@ angular.module('app')
     templateUrl: './templates/comment.html',
     link: function(scope, element, attr) {
       // post new user comment on form submission
+      // console.log('scope', scope);
+      // console.log('element', element);
+      // console.log('attr', attr);
+      // console.log('scope.postId', scope.postId);
       scope.usercomment = function(postId, text) {
         return $http({
           method: 'POST',
           url: '/api/post/comment',
           data: {
-            post_Id: postId,
+            postId: postId,
             text: text,
-            //need to grab current user
-            username: 'moni'
+            //need to change username to user once merged
+            username: $rootScope.username
           }
         })
         .then(function(commentData) {
-          console.log('commentData ', commentData);
+          console.log('commentData: ', commentData);
           console.log('scope.postComments', scope.postComments);
+          // need to push into post with post id
           scope.postComments.push(commentData);
           return scope.postComments;
         })
@@ -31,7 +36,7 @@ angular.module('app')
           console.log('error in sending user comment: ', err)
         })
       };
-            
+    console.log(scope)        
     }
   }
 })
