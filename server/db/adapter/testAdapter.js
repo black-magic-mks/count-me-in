d39a05e1da545661b889b5ba0a4dd69521da40ce
Réal Provencher <real@rrprovencher.com>
2015-07-10 10:04:06 -0700

    [feat]: created reset API route for adding users, pledges, posts, and the basic relationships thereof

--- server/db/adapter/testAdapter.js
+++ server/db/adapter/testAdapter.js
@@ -1,11 +1,117 @@
+var authAdapter = require('../../auth');
+var testData = require('../testData');
+
+var models = require('../models');
+var Pledge = models.Pledge;
+var User = models.User;
+var Post = models.Post;
+
 var db = require('seraph')();
 var Q = require('q');
 var query = Q.nbind(db.query,db);
 
-
-
 var resetData = function(req, res, next) {
+  var deleteRels = 'MATCH ()-[r]->() DELETE r';
+  var deleteNodes = 'MATCH (n) DELETE n';
 
+  // remove old data
+  query(deleteRels)
+  .then(function() {
+    return query(deleteNodes);
+  })
+  .then(function() {
+    console.log('Removed all data from database');
+  })
+  .then(function() {
+    //register users from testData.users
+    var registerPromiseFactory = function(username,password) {
+      var mockReq = { body: {}, session: {} };
+      mockReq.body.username = username;
+      mockReq.body.password = password;
+
+      var deferred = Q.defer();
+      var mockSend = function(val) {
+        if (val) {
+          deferred.resolve(username);
+        } else {
+          deferred.reject('Could not register user:',username);
+        }
+      }
+      var mockRes = { send: mockSend };
+
+      authAdapter.register(mockReq,mockRes,next);
+      return deferred.promise;
+    };
+
+    return Q.all(testData.users.map(function(user) {
+      return registerPromiseFactory(user.username,user.password)
+      .then(function(username) {
+        return username;
+      });
+    }));
+  })
+  .then(function(users) {
+    console.log();
+    console.log('Created users:');
+    console.log(users);
+  })
+  .then(function() {
+    //create pledges from testData.pledges
+    return Q.all(testData.pledges.map(function(pledge) {
+      return Pledge.save(pledge)
+      .then(function(pledgeNode) {
+        //set up user-pledge relationship
+        return Q.all(pledge.subscribers.map(function(username) {
+          return User.where({ username: username })
+          .then(function(userNode) {
+            return Pledge.relate(pledgeNode,'SUBSCRIBES_TO',userNode);
+          });
+        }));
+      })
+      .then(function() {
+        return pledge.pledgename;
+      });
+    }));
+  })
+  .then(function(pledges) {
+    console.log();
+    console.log('Created pledges:');
+    console.log(pledges);
+  })
+  .then(function() {
+    //create posts from testData.posts
+    return Q.all(testData.posts.map(function(post) {
+      return Post.save(post)
+      .then(function(postNode) {
+        //set up user-post-pledge relationships
+        return Q.all([
+          postNode,
+          User.where({ username: post.username }),
+          Pledge.where({ pledgename: post.pledgename })
+        ]);
+      })
+      .spread(function(postNode,user,pledge) {
+        return Q.all([
+          User.relate(user,'POSTED',postNode),
+          Post.relate(postNode,'POSTED_IN',pledge)
+        ]);
+      })
+      .then(function() {
+        return post.title;
+      });
+    }));
+  })
+  .then(function(posts) {
+    console.log();
+    console.log('Created posts:');
+    console.log(posts);
+  })
+  .then(function() {
+    console.log();
+    console.log('Finished resetting database');
+    res.send('Finished resetting database');
+  })
+  .catch(next);
 };
 
 var fillData = function(req, res, next) {
@@ -51,7 +157,7 @@ var clearData = function(req, res, next) {
   var cypherNodes = 'MATCH (n) DELETE n';
 
   query(cypherRels)
-  .then(function(){
+  .then(function() {
     return query(cypherNodes);
   })
   .then(function() {
---
+++ server/db/testData.js
@@ -0,0 +1,339 @@
+module.exports = {
+  users: [
+    {
+      username: 'shahshaank',
+      password: 'Hello1',
+      following: ['mangle9']
+    },
+    {
+      username: 'mangle9',
+      password: 'Hello1',
+      following: ['shahshaank','therealréal','DaGoldberg','CTO_NLinksta']
+    },
+    {
+      username: 'therealréal',
+      password: 'Hello1',
+      following: ['shahshaank','mangle9','DaGoldberg','CTO_NLinksta']
+    },
+    {
+      username: 'DaGoldberg',
+      password: 'Hello1',
+      following: ['mangle9','therealréal','CTO_NLinksta']
+    },
+    {
+      username: 'CTO_NLinksta',
+      password: 'Hello1',
+      following: ['DaGoldberg']
+    },
+  ],
+
+  pledges: [
+    {
+      pledgename: 'beactive',
+      subscribers: ['mangle9','DaGoldberg']
+    },
+    {
+      pledgename: 'WriteFiction',
+      subscribers: []
+    },
+    {
+      pledgename: 'beaDOG',
+      subscribers: ['therealréal','CTO_NLinksta']
+    },
+    {
+      pledgename: 'doPushUps',
+      subscribers: ['mangle9']
+    },
+    {
+      pledgename: 'loseweight',
+      subscribers: []
+    },
+    {
+      pledgename: 'learningtounicycle',
+      subscribers: ['CTO_NLinksta','mangle9']
+    }
+  ],
+
+  posts: [
+    {
+      pledgename: 'beactive',
+      title: 'worked out',
+      text: 'Push press, 5x6-1x12, 65-75-80x3-45\nDb bench press, 5x6, 40-45-50x3\nKettle bell Arnold press, 4x8, 12kg?\n Bosu ball push-ups, 4x10',
+      aws_url: 'http://i.imgur.com/ZDA7HL8.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beactive',
+      title: 'Ran',
+      text: 'ran 8 miles, did 200 pushups, and random abs stuff',
+      aws_url: 'https://i.imgur.com/VsypS7K.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beactive',
+      title: 'Worked Out',
+      text: 'Deadlift, 6x6. 155-165-175-185x3\nLat pull down, 4x8, 115\nSeated row, 4x8, 85-93x3\nBoxing',
+      aws_url: 'https://i.imgur.com/L7SUjW5.png',
+      username: 'DaGoldberg',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beactive',
+      title: 'cardio',
+      text: 'Cardio circuit, x3\nBox jumps\nJump rope\nBig tire toss\nBulgarian bag swing',
+      aws_url: 'https://i.imgur.com/NLe9dXY.png',
+      username: 'DaGoldberg',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beactive',
+      title: 'squat day',
+      text: 'Bench press, 3x6-2x5 with spot, 85-95-105-115x2\nPush press, 6x6, 55-65-75-80-85x2\nIncline db press, 4x8, 35x2-40x2\nArnold press, 4x8, 8kg kettle bell x2 - 20lb x2',
+      aws_url: 'https://i.imgur.com/0l59mVc.jpg',
+      username: 'DaGoldberg',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beactive',
+      title: 'played soccer',
+      text: 'played on the harvard campus!',
+      aws_url: 'https://i.imgur.com/Acr6kBR.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'learningtounicycle',
+      title: '30 minutes',
+      text: 'had a streak for 10 straight minutes',
+      aws_url: 'https://i.imgur.com/h3A3K70.png',
+      username: 'CTO_NLinksta',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'learningtounicycle',
+      title: '20 minutes',
+      text: 'had a streak for 11 straight minutes',
+      aws_url: 'https://i.imgur.com/dxELaOj.png',
+      username: 'CTO_NLinksta',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'learningtounicycle',
+      title: '13 minutes',
+      text: 'had a streak for 20 straight minutes',
+      aws_url: 'https://i.imgur.com/71PoHrz.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'learningtounicycle',
+      title: '14 minutes',
+      text: 'had a streak for 4 straight minutes',
+      aws_url: 'https://i.imgur.com/8OEbCcb.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'doPushUps',
+      title: 'shoulder pushups',
+      text: '3 sets of 40',
+      aws_url: 'https://i.imgur.com/tVRtGDW.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'doPushUps',
+      title: 'regular wide pushups',
+      text: '',
+      aws_url: 'https://i.imgur.com/RhwKpzO.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'doPushUps',
+      title: 'close grip push ups',
+      text: '',
+      aws_url: 'https://i.imgur.com/Xt7SKYh.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'doPushUps',
+      title: 'regular wide pushups',
+      text: '10 sets of 80',
+      aws_url: 'https://i.imgur.com/adzYGgI.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'doPushUps',
+      title: 'close grip push ups',
+      text: '2 sets of 10 wide, 3 sets of 20 close',
+      aws_url: 'https://i.imgur.com/4WHBzt7.png',
+      username: 'mangle9',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beaDOG',
+      title: 'code',
+      text: 'Coded this website',
+      aws_url: 'https://i.imgur.com/1pbtoVN.jpg',
+      username: 'therealréal',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beaDOG',
+      title: 'read a book',
+      text: 'Read "To Bark at Mockingbird" by Barker Lee',
+      aws_url: 'https://i.imgur.com/ZbCn10B.jpg',
+      username: 'therealréal',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beaDOG',
+      title: 'wash a car',
+      text: 'Washed by car',
+      aws_url: 'https://i.imgur.com/3wIZshz.jpg',
+      username: 'therealréal',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beaDOG',
+      title: 'play poker',
+      text: 'Won 20 dog biscuits!',
+      aws_url: 'https://i.imgur.com/2BKi8j7.jpg',
+      username: 'CTO_NLinksta',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    },
+    {
+      pledgename: 'beaDOG',
+      title: 'be a lumberjack',
+      text: 'I\'m a lumberjack',
+      aws_url: 'https://i.imgur.com/11uRZ3T.jpg',
+      username: 'CTO_NLinksta',
+      likes: [],
+      comments: [
+        {
+          text: '',
+          username: ''
+        }
+      ]
+    }
+  ]
+};
