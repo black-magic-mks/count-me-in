40847d758081e855d0a002732e480b324aaa7232
Réal Provencher <real@rrprovencher.com>
2015-07-13 22:17:37 -0700

    [refactor]: fixed code style in seraph.js

--- server/db/seraph.js
+++ server/db/seraph.js
@@ -1,5 +1,6 @@
+var db;
 if (process.env.NODE_ENV === 'production') {
-  var url = require('url').parse(process.env.GRAPHENEDB_URL)
+  var url = require('url').parse(process.env.GRAPHENEDB_URL);
   db = require('seraph')({
     server: url.protocol + '//' + url.host,
     user: url.auth.split(':')[0],
